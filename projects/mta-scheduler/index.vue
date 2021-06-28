ccc03b3400d83da23625d11ddf2d43fcc807e132
Vincenzo Perrone <v.perrone@mindtheapp.it>
2021-06-28 10:05:58 +0200

    Update index.vue

--- projects/mta-scheduler/index.vue
+++ projects/mta-scheduler/index.vue
@@ -1,10 +1,14 @@
 <template>
-  <h3>ciao</h3>
+  <h3>ciao {{test}}</h3>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            test: 'VUE'
+        }
+    }
 }
 </script>
 
