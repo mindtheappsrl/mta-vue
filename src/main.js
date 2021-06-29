20448dc400c0e59df98e485f6a166401766c36ac
Vincenzo Perrone <v.perrone@mindtheapp.it>
2021-06-29 12:02:18 +0200

    .

--- src/main.js
+++ src/main.js
@@ -1,19 +1,3 @@
-// import Vue from 'vue'
-// import vuetify from '@/plugins/vuetify'
-// import App from "./App.vue"
-
-// Vue.config.productionTip = false
-
-// new Vue({
-//   vuetify,
-//   render: h => h(App),
-// }).$mount('#app')
-
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-
-Vue.use(Vuetify)
-
 import MtaScheduler from "./components/MtaScheduler.vue"; 
  
 export {
