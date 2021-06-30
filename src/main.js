06d9e28cfefb8535fccb8b2e1ab164b8b58908b6
Vincenzo Perrone <v.perrone@mindtheapp.it>
2021-06-30 09:37:02 +0200

    refactor

--- src/main.js
+++ src/main.js
@@ -1,20 +1,10 @@
 import Vue from 'vue'
-
-import Vuetify from 'vuetify'
-Vue.use(Vuetify)
-
-// import vuetify from '@/plugins/vuetify'
-// import App from "./App.vue"
-
-// Vue.config.productionTip = false
-
-// new Vue({
-//   vuetify,
-//   render: h => h(App),
-// }).$mount('#app')
-
+import vuetify from '@/plugins/vuetify'
 import MtaScheduler from "./components/MtaScheduler.vue"; 
- 
-export {
-  MtaScheduler
-};
+
+Vue.config.productionTip = false
+
+new Vue({
+  vuetify,
+  render: h => h(MtaScheduler),
+}).$mount('#app')
