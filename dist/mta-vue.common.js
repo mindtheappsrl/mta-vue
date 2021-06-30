744da905c60d33fb3713069cddc4db9481e7d6af
Vincenzo Perrone <v.perrone@mindtheapp.it>
2021-06-30 11:40:03 +0200

    .

--- dist/mta-vue.common.js
+++ dist/mta-vue.common.js
@@ -68122,7 +68122,7 @@ var vuetify_default = /*#__PURE__*/__webpack_require__.n(vuetify);
 // EXTERNAL MODULE: ./node_modules/vuetify/dist/vuetify.min.css
 var vuetify_min = __webpack_require__("bf40");
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"72b493a2-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MtaScheduler.vue?vue&type=template&id=5a10b58e&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"7d3a2cef-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MtaScheduler.vue?vue&type=template&id=5a10b58e&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticStyle:{"width":"100%","overflow":"auto"}},[_c('div',{ref:"schedulerContainer",staticStyle:{"position":"relative"}},[_c('v-toolbar',{staticClass:"grey lighten-4",class:{
         'mr-4':
           (_vm.categories.length > 5 && _vm.typeValue === 'category') || _vm.typeValue === 'week',
