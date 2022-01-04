96da352f8302259959fd168f4fbaa960139fede6
AleTornesello <alessandro.tornesello99@gmail.com>
2022-01-04 15:39:50 +0100

    0.4.6

--- dist/mta-vue.umd.js
+++ dist/mta-vue.umd.js
@@ -70639,7 +70639,7 @@ var vuetify_default = /*#__PURE__*/__webpack_require__.n(vuetify);
 // EXTERNAL MODULE: ./node_modules/vuetify/dist/vuetify.min.css
 var vuetify_min = __webpack_require__("bf40");
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"799f4888-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MtaScheduler.vue?vue&type=template&id=1a52dbc7&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"26b897ae-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MtaScheduler.vue?vue&type=template&id=1a52dbc7&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticStyle:{"position":"relative"}},[_c('v-toolbar',{staticClass:"grey lighten-4",attrs:{"id":"toolbar","dense":"","flat":""}},[_c('v-btn',{attrs:{"rounded":"","icon":""},on:{"click":function($event){return _vm.prev()}}},[_c('v-icon',[_vm._v("mdi-chevron-left")])],1),_c('h3',{staticClass:"text--secondary text-center",staticStyle:{"min-width":"210px"}},[_vm._v(" "+_vm._s(_vm.moment(_vm.currentDate).format("dddd DD MMMM YYYY"))+" ")]),_c('v-btn',{attrs:{"rounded":"","icon":""},on:{"click":function($event){return _vm.next()}}},[_c('v-icon',[_vm._v("mdi-chevron-right")])],1),_c('v-btn-toggle',{staticClass:"ml-5",attrs:{"mandatory":"","rounded":"","dense":"","color":"secondary darken-2"},model:{value:(_vm.typeValue),callback:function ($$v) {_vm.typeValue=$$v},expression:"typeValue"}},[_c('v-btn',{attrs:{"value":_vm.hasCategoryView ? 'category' : 'day'}},[_vm._v(" Giorno ")]),_c('v-btn',{attrs:{"value":"week"}},[_vm._v(" Settimana ")]),_c('v-btn',{attrs:{"value":"month"}},[_vm._v(" Mese ")])],1),(_vm.typeValue != 'month')?_c('v-switch',{staticClass:"ml-5 mt-5",attrs:{"inset":"","color":"primary","label":"Mostra intera giornata"},on:{"change":_vm.setCalendarViewInterval},model:{value:(_vm.showFullDay),callback:function ($$v) {_vm.showFullDay=$$v},expression:"showFullDay"}}):_vm._e(),(_vm.typeValue == 'category' && !_vm.proposalData && _vm.extraBtnData.visible)?_c('v-btn',{staticClass:"ml-5",attrs:{"elevation":"0","rounded":"","color":"orange","dark":""},on:{"click":_vm.extraBtnData.fn}},[_vm._v(" "+_vm._s(_vm.extraBtnData.text)+" ")]):_vm._e()],1),(_vm.showTopScrollbar && _vm.typeValue !== 'category' && _vm.typeValue !== 'day')?_c('MtaHorizontalScrollbar',{ref:"horizontalScrollbar",attrs:{"container-ref":_vm.horizontalScheduleScrollbar.container}}):_vm._e(),_c('div',{ref:"scheduleScrollbarRef",staticStyle:{"width":"100%","overflow":"auto"}},[_c('div',{ref:"schedulerContainer"},[_c('v-calendar',_vm._g({ref:"calendar",attrs:{"locale":_vm.localeValue,"type":_vm.typeValue,"events":_vm.eventsData,"event-overlap-mode":_vm.modeValue,"event-overlap-threshold":30,"event-color":_vm.getEventColor,"first-time":_vm.firstTimeValue,"interval-count":_vm.intervalCountValue,"interval-minutes":_vm.intervalMinutesValue,"interval-format":function (locale, getOptions) { return locale.time; },"mobile-breakpoint":0,"event-more":false,"categories":_vm.categories,"weekdays":[1, 2, 3, 4, 5, 6, 0]},on:{"change":_vm.getEvents,"click:event":_vm.showDetails,"click:date":_vm.goToDate,"click:interval":_vm.emitDateTime},scopedSlots:_vm._u([{key:"event",fn:function(ref){
 var event = ref.event;
 return [_c('v-tooltip',{attrs:{"top":""},scopedSlots:_vm._u([{key:"activator",fn:function(ref){
@@ -71356,12 +71356,12 @@ var component = normalizeComponent(
 )
 
 /* harmony default export */ var MtaScheduler = (component.exports);
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"799f4888-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MtaHorizontalScrollbar.vue?vue&type=template&id=0c02e2fc&
-var MtaHorizontalScrollbarvue_type_template_id_0c02e2fc_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{ref:"fakeScrollbar",attrs:{"id":"fakeScrollbar"}},[_c('div',{ref:"fakeScrollbarContent"},[_vm._v(" ")])])}
-var MtaHorizontalScrollbarvue_type_template_id_0c02e2fc_staticRenderFns = []
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"26b897ae-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MtaHorizontalScrollbar.vue?vue&type=template&id=203ffcb8&
+var MtaHorizontalScrollbarvue_type_template_id_203ffcb8_render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{ref:"fakeScrollbar",attrs:{"id":"fakeScrollbar"}},[_c('div',{ref:"fakeScrollbarContent"},[_vm._v(" ")])])}
+var MtaHorizontalScrollbarvue_type_template_id_203ffcb8_staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/MtaHorizontalScrollbar.vue?vue&type=template&id=0c02e2fc&
+// CONCATENATED MODULE: ./src/components/MtaHorizontalScrollbar.vue?vue&type=template&id=203ffcb8&
 
 // CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js??ref--12-0!./node_modules/thread-loader/dist/cjs.js!./node_modules/babel-loader/lib!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/MtaHorizontalScrollbar.vue?vue&type=script&lang=js&
 //
@@ -71388,26 +71388,26 @@ var MtaHorizontalScrollbarvue_type_template_id_0c02e2fc_staticRenderFns = []
     }
   },
   mounted: function mounted() {
-    if (this.$refs.fakeScrollbar && this.$refs.fakeScrollbarContent && this.containerRef) {
-      this.initScrollbar();
-    }
+    this.initScrollbar();
   },
   methods: {
     initScrollbar: function initScrollbar() {
       var _this = this;
 
-      // Set width of fake scrollbar content to the width of the container
-      this.$refs.fakeScrollbarContent.style.width = this.containerRef.children[0].offsetWidth + "px"; // Set event listener for scroll left event
+      if (this.$refs.fakeScrollbar && this.$refs.fakeScrollbarContent && this.containerRef) {
+        // Set width of fake scrollbar content to the width of the container
+        this.$refs.fakeScrollbarContent.style.width = this.containerRef.children[0].offsetWidth + "px"; // Set event listener for scroll left event
 
-      this.$nextTick(function () {
-        _this.$refs.fakeScrollbar.onscroll = function () {
-          _this.containerRef.scrollLeft = _this.$refs.fakeScrollbar.scrollLeft;
-        };
+        this.$nextTick(function () {
+          _this.$refs.fakeScrollbar.onscroll = function () {
+            _this.containerRef.scrollLeft = _this.$refs.fakeScrollbar.scrollLeft;
+          };
 
-        _this.containerRef.onscroll = function () {
-          _this.$refs.fakeScrollbar.scrollLeft = _this.containerRef.scrollLeft;
-        };
-      });
+          _this.containerRef.onscroll = function () {
+            _this.$refs.fakeScrollbar.scrollLeft = _this.containerRef.scrollLeft;
+          };
+        });
+      }
     },
     updateScrollbar: function updateScrollbar() {
       this.initScrollbar();
@@ -71430,8 +71430,8 @@ var MtaHorizontalScrollbarvue_type_style_index_0_lang_css_ = __webpack_require__
 
 var MtaHorizontalScrollbar_component = normalizeComponent(
   components_MtaHorizontalScrollbarvue_type_script_lang_js_,
-  MtaHorizontalScrollbarvue_type_template_id_0c02e2fc_render,
-  MtaHorizontalScrollbarvue_type_template_id_0c02e2fc_staticRenderFns,
+  MtaHorizontalScrollbarvue_type_template_id_203ffcb8_render,
+  MtaHorizontalScrollbarvue_type_template_id_203ffcb8_staticRenderFns,
   false,
   null,
   null,
